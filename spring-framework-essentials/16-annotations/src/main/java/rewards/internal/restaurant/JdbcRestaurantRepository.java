b4a53151ab00fc557e656eeb171ee93c90bf296c
Vladimir Cucu <cucu.vova@gmail.com>
2026-08-19 15:19:08 +0300

    test: implement jdbcrestaurantrepository init and destroy callbacks

--- spring-framework-essentials/16-annotations/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
+++ spring-framework-essentials/16-annotations/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,24 +20,6 @@ import java.util.Map;
  * <p>
  * This implementation should cache restaurants to improve performance. The
  * cache should be populated on initialization and cleared on destruction.
- */
-
-/*
- * TODO-08: Use Setter injection for DataSource
- * - Change the configuration to set the dataSource
- *   property using setDataSource().
- *
- *   To do this, you must MOVE the @Autowired annotation
- *   you might have set in the previous step on the
- *   constructor injecting DataSource.
- *   So neither constructor should be annotated with
- *   @Autowired now, so Spring uses
- *   the default constructor by default.
- *
- * - Re-run the test. It should fail.
- * - Examine the stack trace and see if you can
- *   understand why. (If not, refer to lab document).
- *   We will fix this error in the next step.
  */
 
 @Service
@@ -54,15 +38,14 @@ public class JdbcRestaurantRepository implements RestaurantRepository {
 	 * restaurants. When the instance of JdbcRestaurantRepository is created, a
 	 * Restaurant cache is populated for read only access
 	 */
-	@Autowired
 	public JdbcRestaurantRepository(DataSource dataSource) {
 		this.dataSource = dataSource;
-		this.populateRestaurantCache();
 	}
 
 	public JdbcRestaurantRepository() {
 	}
 
+	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
@@ -77,16 +60,7 @@ public class JdbcRestaurantRepository implements RestaurantRepository {
 	 * by their merchant numbers. This method should be called on initialization.
 	 */
 
-	/*
-	 * TODO-09: Make this method to be invoked after a bean gets created
-	 * - Mark this method with an annotation that will cause it to be
-	 *   executed by Spring after constructor & setter initialization has occurred.
-	 * - Re-run the RewardNetworkTests test. You should see the test succeeds.
-	 * - Note that populating the cache is not really a valid
-	 *   construction activity, so using a post-construct, rather than
-	 *   the constructor, is a better practice.
-	 */
-
+	@PostConstruct
 	void populateRestaurantCache() {
 		restaurantCache = new HashMap<String, Restaurant>();
 		String sql = "select MERCHANT_NUMBER, NAME, BENEFIT_PERCENTAGE from T_RESTAURANT";
@@ -147,19 +121,10 @@ public class JdbcRestaurantRepository implements RestaurantRepository {
 	/**
 	 * Helper method that clears the cache of restaurants.
 	 * This method should be called when a bean is destroyed.
-	 * <p>
-	 * TODO-10: Add a scheme to check if this method is being invoked
-	 * - Add System.out.println to this method.
-	 * <p>
-	 * TODO-11: Have this method to be invoked before a bean gets destroyed
-	 * - Re-run RewardNetworkTests.
-	 * - Observe this method is not called.
-	 * - Use an appropriate annotation to register this method for a
-	 *   destruction lifecycle callback.
-	 * - Re-run the test and you should be able to see
-	 *   that this method is now being called.
 	 */
+	@PreDestroy
 	public void clearRestaurantCache() {
+		System.out.println("clearRestaurantCache invoked");
 		restaurantCache.clear();
 	}
 
