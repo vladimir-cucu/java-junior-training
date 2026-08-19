374a86a73ad753c17365bd101e6f588017c62b15
Vladimir Cucu <cucu.vova@gmail.com>
2026-08-19 09:46:07 +0300

    test: implement rewardsconfigtests unit test

--- spring-framework-essentials/12-javaconfig-dependency-injection/src/test/java/config/RewardsConfigTests.java
+++ spring-framework-essentials/12-javaconfig-dependency-injection/src/test/java/config/RewardsConfigTests.java
@@ -1,11 +1,21 @@
 package config;
 
 import org.assertj.core.api.Fail;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
 
 import javax.sql.DataSource;
 import java.lang.reflect.Field;
 
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
@@ -15,40 +25,32 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SuppressWarnings("unused")
 public class RewardsConfigTests {
 	// Provide a mock object for testing
-	private DataSource dataSource = Mockito.mock(DataSource.class);
+	final private DataSource dataSource = Mockito.mock(DataSource.class);
 
-	// TODO-05: Run the test
-	// - Uncomment the code below between /* and */
-	// - If you have implemented RewardsConfig as requested it should compile.
-	// - Fix RewardsConfig if necessary.
-	// - Now run the test, it should pass.
-
-	/*
-	private RewardsConfig rewardsConfig = new RewardsConfig(dataSource);
+	final private RewardsConfig rewardsConfig = new RewardsConfig(dataSource);
 
 	@Test
 	public void getBeans() {
 		RewardNetwork rewardNetwork = rewardsConfig.rewardNetwork();
-		assertTrue(rewardNetwork instanceof RewardNetworkImpl);
+		assertInstanceOf(RewardNetworkImpl.class, rewardNetwork);
 
 		AccountRepository accountRepository = rewardsConfig.accountRepository();
-		assertTrue(accountRepository instanceof JdbcAccountRepository);
+		assertInstanceOf(JdbcAccountRepository.class, accountRepository);
 		checkDataSource(accountRepository);
 
 		RestaurantRepository restaurantRepository = rewardsConfig.restaurantRepository();
-		assertTrue(restaurantRepository instanceof JdbcRestaurantRepository);
+		assertInstanceOf(JdbcRestaurantRepository.class, restaurantRepository);
 		checkDataSource(restaurantRepository);
 
 		RewardRepository rewardsRepository = rewardsConfig.rewardRepository();
-		assertTrue(rewardsRepository instanceof JdbcRewardRepository);
+		assertInstanceOf(JdbcRewardRepository.class, rewardsRepository);
 		checkDataSource(rewardsRepository);
 	}
-	*/
 
 	/**
 	 * Ensure the data-source is set for the repository. Uses reflection as we do
 	 * not wish to provide a getDataSource() method.
-	 * 
+	 *
 	 * @param repository One of our three repositories.
 	 *
 	 */
