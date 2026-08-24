3c4e62dddf3271599d6280e78d2304cd05a17799
Vladimir Cucu <cucu.vova@gmail.com>
2026-08-24 12:41:44 +0300

    chore: create 30-jdbc-boot project using spring initializr

---
+++ spring-boot/30-jdbc-boot/src/main/java/rewards/JdbcBootApplication.java
@@ -0,0 +1,13 @@
+package rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcBootApplication.class, args);
+	}
+
+}
