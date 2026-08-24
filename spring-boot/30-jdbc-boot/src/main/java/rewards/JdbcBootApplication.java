d4e33aebb38efba72243978d1fe1dabce2e3ffba
Vladimir Cucu <cucu.vova@gmail.com>
2026-08-24 13:00:43 +0300

    feat: use commandlinerunner bean to show number of accounts

--- spring-boot/30-jdbc-boot/src/main/java/rewards/JdbcBootApplication.java
+++ spring-boot/30-jdbc-boot/src/main/java/rewards/JdbcBootApplication.java
@@ -1,7 +1,10 @@
 package rewards;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class JdbcBootApplication {
@@ -10,4 +13,12 @@ public class JdbcBootApplication {
 		SpringApplication.run(JdbcBootApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+		final String query = "SELECT COUNT(*) FROM T_ACCOUNT";
+
+		return args -> System.out.println("Hello, there are "
+				+ jdbcTemplate.queryForObject(query, Integer.class)
+				+ " accounts");
+	}
 }
