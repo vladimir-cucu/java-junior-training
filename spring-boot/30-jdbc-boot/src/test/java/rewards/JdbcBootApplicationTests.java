89a242a6f8c056af5a45375dceb6c182382defd5
Vladimir Cucu <cucu.vova@gmail.com>
2026-08-24 14:13:41 +0300

    test: verify number of accounts

--- spring-boot/30-jdbc-boot/src/test/java/rewards/JdbcBootApplicationTests.java
+++ spring-boot/30-jdbc-boot/src/test/java/rewards/JdbcBootApplicationTests.java
@@ -1,13 +1,22 @@
 package rewards;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JdbcBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
+	@Test
+	public void testNumberOfAccounts() {
+		final String query = "SELECT COUNT(*) FROM T_ACCOUNT";
+		int numberOfAccounts = jdbcTemplate.queryForObject(query, Integer.class);
+		assertThat(numberOfAccounts).isEqualTo(21);
+	}
 }
