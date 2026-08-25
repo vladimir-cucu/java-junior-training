3c3cf1edcc7f128db983d12f674e09f4a202a84c
Vladimir Cucu <cucu.vova@gmail.com>
2026-08-25 11:00:27 +0300

    feat: disable datasource auto-configuration

--- spring-boot/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ spring-boot/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -1,29 +1,19 @@
 package rewards;
 
+import config.RewardsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-// TODO-11 (Optional) : Disable 'DataSource' auto-configuration
-// - Note that you are using your own 'DataSource' bean now
-//   instead of auto-configured one
-// - Use 'exclude' attribute of '@SpringBootApplication'
-//   excluding 'DataSourceAutoConfiguration' class
-// - Run this application and observe a failure
-// - Import 'RewardsConfig' class
-// - Run this application again and observe a successful execution
-
-// TODO-12 (Optional) : Look in application.properties for the next step.
-
-// TODO-13 (Optional) : Follow the instruction in the lab document.
-//           The section titled "Build and Run using Command Line tools".
-
-@SpringBootApplication
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+@Import(RewardsConfig.class)
 @EnableConfigurationProperties(RewardsRecipientProperties.class)
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
