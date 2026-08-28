3ef6bb581d9de49b337663183979d00b8085fdcd
Vladimir Cucu <cucu.vova@gmail.com>
2026-08-28 16:19:46 +0300

    test: test add and delete beneficiary endpoint

--- spring-boot/38-rest-ws/src/test/java/accounts/client/AccountClientTests.java
+++ spring-boot/38-rest-ws/src/test/java/accounts/client/AccountClientTests.java
@@ -63,33 +63,19 @@ public class AccountClientTests {
 	}
 
 	@Test
-	@Disabled
 	public void addAndDeleteBeneficiary() {
-		// perform both add and delete to avoid issues with side effects
+		URI location = restTemplate.postForLocation(BASE_URL + "/accounts/1/beneficiaries", "David");
 
-		// TODO-13: Create a new Beneficiary
-		// - Remove the @Disabled on this test method.
-		// - Create a new Beneficiary called "David" for the account with id 1
-		//	 (POST the String "David" to the "/accounts/{accountId}/beneficiaries" URL).
-		// - Store the returned location URI in a variable.
-
-		// TODO-14: Retrieve the Beneficiary you just created from the location that was returned
-		Beneficiary newBeneficiary = null; // Modify this line to use the restTemplate
+		Beneficiary newBeneficiary = restTemplate.getForObject(location.toString(), Beneficiary.class);
 
 		assertNotNull(newBeneficiary);
 		assertEquals("David", newBeneficiary.getName());
 
-		// TODO-15: Delete the newly created Beneficiary
-
+		restTemplate.delete(location.toString());
 
 		HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
 			System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
-
-			// TODO-16: Try to retrieve the newly created Beneficiary again.
-			// - Run this test, then. It should pass because we expect a 404 Not Found
-			//   If not, it is likely your delete in the previous step
-			//   was not successful.
-
+			restTemplate.getForObject(location.toString(), Beneficiary.class);
 		});
 		assertEquals(HttpStatus.NOT_FOUND, httpClientErrorException.getStatusCode());
 	}
