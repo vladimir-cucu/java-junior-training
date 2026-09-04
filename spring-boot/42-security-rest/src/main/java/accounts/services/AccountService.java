5f986eb82c4c1cd7bd4337da5f40be29f21001ae
vladimir-cucu <cucu.vova@gmail.com>
2026-09-04 16:41:17 +0300

    feat: retrieve user authorities from securitycontext

--- spring-boot/42-security-rest/src/main/java/accounts/services/AccountService.java
+++ spring-boot/42-security-rest/src/main/java/accounts/services/AccountService.java
@@ -1,6 +1,7 @@
 package accounts.services;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -24,19 +25,8 @@ public class AccountService {
     //
     //@PreAuthorize(/* Add code here */)
     public List<String> getAuthoritiesForUser(String username) {
-
-        // TODO-08: Retrieve authorities (roles) for the logged-in user
-        // (This is probably not a typical business logic you will
-        //  have in a service layer. This is mainly to show
-        //  how SecurityContext object is maintained in the local
-        //  thread, which can be accessed via SecurityContextHolder)
-        // - Replace null below with proper code - use SecurityContextHolder
-        // - Restart the application (or let Spring Boot Devtools to restart the app)
-        // - Using Chrome Incognito browser or "curl", access
-        //   http://localhost:8080/authorities?username=<username>
-        // - Verify that roles of the logged-in user get displayed
         Collection<? extends GrantedAuthority> grantedAuthorities
-                = null; // Modify this line
+                = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
 
         return grantedAuthorities.stream()
                                  .map(GrantedAuthority::getAuthority)
