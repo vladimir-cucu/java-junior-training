08e04c7758f975ed776193904f39256c00635a18
vladimir-cucu <cucu.vova@gmail.com>
2026-09-04 16:46:03 +0300

    feat: add preauthorize check for authority retrieval

--- spring-boot/42-security-rest/src/main/java/accounts/services/AccountService.java
+++ spring-boot/42-security-rest/src/main/java/accounts/services/AccountService.java
@@ -1,5 +1,6 @@
 package accounts.services;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -11,19 +12,7 @@ import java.util.stream.Collectors;
 @Service
 public class AccountService {
 
-    // TODO-09: Add method security annotation to a method
-    // - Uncomment and complete PreAuthorize annotation below
-    //   so that the method is permitted to be invoked only
-    //   when both of following two run-time conditions are met:
-    //   (Use SpEL to specify these conditions.)
-    //
-    //   (a) the logged-in user belongs to "ADMIN" role
-    //   (b) the value of the "username" argument matches
-    //       the value of the logged-in principal's
-    //       username, which can be accessed as
-    //       principal.username or authentication.name.
-    //
-    //@PreAuthorize(/* Add code here */)
+    @PreAuthorize("hasRole('ADMIN') && #username == principal.username")
     public List<String> getAuthoritiesForUser(String username) {
         Collection<? extends GrantedAuthority> grantedAuthorities
                 = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
