282e1cd5b45c9927f841646c6639d8afde120a1f
vladimir-cucu <cucu.vova@gmail.com>
2026-09-08 10:57:27 +0300

    feat: secure actuator endpoint

--- spring-boot/44-actuator/src/test/java/accounts/client/AccountClientSecurityTests.java
+++ spring-boot/44-actuator/src/test/java/accounts/client/AccountClientSecurityTests.java
@@ -11,10 +11,6 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.*;
 
-// TODO-24: Run the tests checking security configuration for the actuator endpoints
-// - Take some time to understand what each test is for
-// - Remove @Disabled annotation from each test and run it
-// - Make sure all tests pass
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AccountClientSecurityTests {
 
@@ -22,7 +18,6 @@ public class AccountClientSecurityTests {
     private TestRestTemplate restTemplate;
 
     @Test
-    @Disabled
     public void any_user_can_access_health_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.getForEntity("/actuator/health", String.class);
@@ -30,7 +25,6 @@ public class AccountClientSecurityTests {
     }
 
     @Test
-    @Disabled
     public void any_user_can_access_info_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.getForEntity("/actuator/info", String.class);
@@ -38,7 +32,6 @@ public class AccountClientSecurityTests {
     }
 
     @Test
-    @Disabled
     public void any_user_cannot_access_conditions_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.withBasicAuth("anyuser", "anyuser")
@@ -47,7 +40,6 @@ public class AccountClientSecurityTests {
     }
 
     @Test
-    @Disabled
     public void user_in_ADMIN_role_can_access_conditions_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.withBasicAuth("admin", "admin")
@@ -56,7 +48,6 @@ public class AccountClientSecurityTests {
     }
 
     @Test
-    @Disabled
     public void user_in_ACTUATOR_role_cannot_access_conditions_endpoint() {
         ResponseEntity<String> responseEntity
                 = restTemplate.withBasicAuth("actuator", "actuator")
