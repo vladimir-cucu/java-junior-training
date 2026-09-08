616b4a8463b9991614436abc49c3c4585c152137
vladimir-cucu <cucu.vova@gmail.com>
2026-09-08 16:40:29 +0300

    feat: access actuator endpoints using jmx

--- spring-boot/44-actuator/src/main/java/accounts/ActuatorApplication.java
+++ spring-boot/44-actuator/src/main/java/accounts/ActuatorApplication.java
@@ -17,20 +17,3 @@ public class ActuatorApplication {
 	}
 
 }
-
-/*
- * TODO-27 (Optional): Access Actuator endpoints using JMX
- * (If you are short on time, skip this step.)
- * - Add "spring.jmx.enabled=true" to the "application.properties"
- * - Restart the application
- * - In a terminal window, run "jconsole" (from <JDK-directory>/bin)
- * - Select "accounts.ActuatorApplication" under "Local Process"
- *   then click "Connect"
- * - Click "insecure connection" if prompted
- * - Select the MBeans tab, find the "org.springframework.boot"
- *   folder, then open the "Endpoint" sub-folder
- * - Note that all the actuator endpoints ARE exposed for JMX
- * - Expand Health->Operations->health
- * - Click "health" button on the top right pane
- * - Observe the health data gets displayed
- */
