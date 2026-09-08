3fe98a1193713d063cb9a684b376bbd0df42645a
vladimir-cucu <cucu.vova@gmail.com>
2026-09-08 12:25:56 +0300

    feat: use aop for counting accountSummary calls

--- spring-boot/44-actuator/src/main/java/accounts/web/AccountAspect.java
+++ spring-boot/44-actuator/src/main/java/accounts/web/AccountAspect.java
@@ -1,17 +1,23 @@
 package accounts.web;
 
-/*
- * TODO-26 (Optional): Use AOP for counting logic
- * - Add `spring-boot-starter-aop` starter to the `pom.xml` or the
- *   `build.gradle`. (You might want to refresh your IDE so that
- *   it picks up the change in the `pom.xml` or the `build.gradle` file.)
- * - Make this class an Aspect, through which `account.fetch` counter,
- *   which has a tag of `type`/`fromAspect` key/value pair, gets incremented
- *   every time `accountSummary` method of the `AccountController` class
- *   is invoked
- * - Make this a component by using a proper annotation
- * - Access `/accounts` several times and verify the metrics of
- *   `/actuator/metrics/account.fetch?tag=type:fromAspect
- */
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class AccountAspect {
+
+    private final Counter counter;
+
+    public AccountAspect(MeterRegistry meterRegistry) {
+        this.counter = meterRegistry.counter("account.fetch", "type", "fromAspect");
+    }
+
+    @Before("execution(* accounts.web.AccountController.accountSummary())")
+    public void countAccountSummaryCalls() {
+        counter.increment();
+    }
 }
