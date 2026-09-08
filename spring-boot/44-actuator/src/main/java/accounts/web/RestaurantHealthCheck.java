df234d6a4be029806dd2c331fb616ee609b3512a
vladimir-cucu <cucu.vova@gmail.com>
2026-09-08 11:51:14 +0300

    feat: improve restaurant count health check

--- spring-boot/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ spring-boot/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -17,25 +17,11 @@ public class RestaurantHealthCheck implements HealthIndicator {
 
     @Override
     public Health health() {
+        final long restaurantCount = restaurantRepository.getRestaurantCount();
         if (restaurantRepository.getRestaurantCount() > 0) {
             return Health.up().build();
         } else {
-            return Health.down().build();
+            return Health.status("NO_RESTAURANTS").withDetail("restaurantCount", 0).build();
         }
     }
 }
-
-/**
- * TODO-25 (Optional): Experiment with HealthIndicator above
- * - Change "spring.sql.init.data-locations" property in the
- *   "application.properties" file back to use "data-no-restaurants.sql"
- * - Include the restaurant count as extra detail when DOWN state.
- * - Instead of returning DOWN when there are no restaurants,
- *   define and use a custom status called NO_RESTAURANTS.
- * - When there are no restaurants in the DB, what top-level status
- *   is returned for the "application" health group?
- * - Set "management.endpoint.health.group.application.status.order" property
- *   in the "application.properties" file so that NO_RESTAURANTS
- *   gets displayed as top-level status for the "application" health group.
- * - Restart the application and verify the result.
- */
